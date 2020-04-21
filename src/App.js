d78fe3647881d39872d5bd7534a3774e60e1b7cc
aucoeur <aucoeur.ngo@students.makeschool.com>
2020-04-21 12:02:26 -0700

    dynamic content and footer

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import logo from './logo.svg';
 import './App.css';
 import Title from './Title';
 import ItemList from './ItemList';
+import Footer from './Footer';
 
 function App() {
   return (
     <div className="App">
       <Title />
       <ItemList />
+      <Footer />
     </div>
   );
 }
---
+++ src/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './Footer.css';
+
+function Footer() {
+    return (
+        <div className="Footer">
+            STINE © 2020
+        </div>
+    )
+}
+
+export default Footer
