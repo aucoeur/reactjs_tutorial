345d58fbc29865adb020029c1fe813a055ae2b2b
aucoeur <aucoeur.ngo@students.makeschool.com>
2020-04-21 11:15:00 -0700

    Header component built

---
+++ src/Title.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Title.css";
+
+function Title() {
+  return (
+    <div className="Title">
+      <header>
+        <h1>THIS IS TITLE</h1>
+        <div className="Title-Subtitle">This is subtitular!</div>
+      </header>
+    </div>
+  );
+}
+
+export default Title;
